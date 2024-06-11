831e6c221b75691b47761e2390a1705951be4eb0
Steve Clark <steve@bigsteve.us>
2024-06-11 04:42:54 -0400

    add encryption to cloudtrail

--- modules/cloudtrail-baseline/migrations.tf
+++ modules/cloudtrail-baseline/migrations.tf
@@ -3,10 +3,10 @@
 # Removing `enabled` argument.
 # --------------------------------------------------------------------------------------------------
 
-moved {
-  from = aws_kms_key.cloudtrail[0]
-  to   = aws_kms_key.cloudtrail
-}
+# moved {
+#   from = aws_kms_key.cloudtrail[0]
+#   to   = aws_kms_key.cloudtrail
+# }
 
 moved {
   from = aws_cloudtrail.global[0]
